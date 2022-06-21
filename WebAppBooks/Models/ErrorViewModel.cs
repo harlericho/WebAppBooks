2515efb656a00349f7e5b6db8ec503f9128b7102
Charlie <harlericho@gmail.com>
2022-06-20 20:17:30 -0500

    Add project files.

---
+++ WebAppBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
